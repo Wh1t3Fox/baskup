ff834e945fae528ceefcbaffbd2d44e068ad7628
wh1t3fox <dev@exploit.design>
2021-01-24 11:10:43 -0500

    remove attachment, and allow arbitrary path for db. works on *nix

--- baskup.sh
+++ baskup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
+
+[[ $DEBUG ]] && set -x
+
 BACKUP_DIR=./backup
 
-OS_Version=$(sw_vers -productVersion)
-LAST_VERSION=10.13
-NEEDS_MODIFICATION=$(echo $OS_Version '>=' $LAST_VERSION | bc -l)
-BACKUP_ATTACHMENTS=0
-
-while getopts ":a" opt; do
+while getopts "d:a" opt; do
   case $opt in
-    a)
-      echo "Running baskup for text + attachments"
-      BACKUP_ATTACHMENTS=1
+    d)
+      DB_FILE=$OPTARG
+      echo "Using $DB_FILE"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -20,11 +18,10 @@ while getopts ":a" opt; do
 done
 
 function select_rows () {
-  sqlite3 ~/Library/Messages/chat.db "$1"
+  sqlite3 $DB_FILE "$1"
 }
 
 for line in $(select_rows "select distinct guid from chat;" ); do
-
   contact=$line
   arrIN=(${contact//;/ })
   contactNumber=${arrIN[2]}
@@ -32,28 +29,9 @@ for line in $(select_rows "select distinct guid from chat;" ); do
   mkdir -p $BACKUP_DIR/$contactNumber/Attachments
 
   #Perform SQL operations
-  if [[ $NEEDS_MODIFICATION == 1 ]]; then
-    select_rows "
-    select is_from_me,text, datetime((date/1000000000) + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date from message where handle_id=(
-    select handle_id from chat_handle_join where chat_id=(
-    select ROWID from chat where guid='$line')
-    )" | sed 's/1\|/Me: /g;s/0\|/Friend: /g' > $BACKUP_DIR/$contactNumber/$line.txt
-  else
-    select_rows "
-    select is_from_me,text, datetime(date + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date from message where handle_id=(
-    select handle_id from chat_handle_join where chat_id=(
-    select ROWID from chat where guid='$line')
-    )" | sed 's/1\|/Me: /g;s/0\|/Friend: /g' > $BACKUP_DIR/$contactNumber/$line.txt
-  fi
-
-  if [[ $BACKUP_ATTACHMENTS == 1 ]]; then
-    select_rows "
-    select filename from attachment where rowid in (
-    select attachment_id from message_attachment_join where message_id in (
-    select rowid from message where cache_has_attachments=1 and handle_id=(
-    select handle_id from chat_handle_join where chat_id=(
-    select ROWID from chat where guid='$line')
-    )))" | cut -c 2- | awk -v home=$HOME '{print home $0}' | tr '\n' '\0' | xargs -0 -I fname cp fname $BACKUP_DIR/$contactNumber/Attachments
-  fi
-
+  select_rows "
+  select is_from_me,text, datetime(date + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date from message where handle_id=(
+  select handle_id from chat_handle_join where chat_id=(
+  select ROWID from chat where guid='$line')
+  )" | sed 's/1|/Me: /g;s/0|/Friend: /g;s/|$//' > $BACKUP_DIR/$contactNumber/$line.txt
 done

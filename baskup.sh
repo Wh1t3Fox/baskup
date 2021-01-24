3dcb7c6f7d5ac66e96e959e6dd74c38df8e7fa87
wh1t3fox <dev@exploit.design>
2021-01-24 12:02:45 -0500

    fix datetime

--- baskup.sh
+++ baskup.sh
@@ -30,8 +30,8 @@ for line in $(select_rows "select distinct guid from chat;" ); do
 
   #Perform SQL operations
   select_rows "
-  select is_from_me,text, datetime(date + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date from message where handle_id=(
+  select is_from_me,text, datetime(date/1000000000 + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date from message where handle_id=(
   select handle_id from chat_handle_join where chat_id=(
   select ROWID from chat where guid='$line')
-  )" | sed 's/1|/Me: /g;s/0|/Friend: /g;s/|$//' > $BACKUP_DIR/$contactNumber/$line.txt
+  )" | sed 's/1|/Me: /g;s/0|/Friend: /g' > $BACKUP_DIR/$contactNumber/$line.txt
 done
